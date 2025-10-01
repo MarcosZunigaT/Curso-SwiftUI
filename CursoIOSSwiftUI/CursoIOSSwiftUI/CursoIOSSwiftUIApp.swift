7b7e6e164888258ef4c577e6b8ec4e946b7124e2
Marcos Zúniga <37557200+MarcosZunigaT@users.noreply.github.com>
2025-10-01 17:20:23 -0600

    Implementada UI Geometrica

---
+++ CursoIOSSwiftUI/CursoIOSSwiftUI/CursoIOSSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoIOSSwiftUIApp.swift
+//  CursoIOSSwiftUI
+//
+//  Created by Marcos Antonio Zúniga Tapia on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoIOSSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
